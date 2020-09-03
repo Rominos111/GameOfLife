a286988d55f966f18807e1ca864da3253dd4c450
Romain <romain.theodet@hotmail.fr>
2020-09-03 16:53:48 +0200

    new: Commit initial

---
+++ include/main.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 03/09/2020.
+
+#ifndef GAMEOFLIFE_MAIN_H
+#define GAMEOFLIFE_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif //GAMEOFLIFE_MAIN_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Created by Romain on 03/09/2020.
+
+#include "main.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
