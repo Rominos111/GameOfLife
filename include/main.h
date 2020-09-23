7f1e13e3d1ec852687d2c5d2e75c8d9064e86c34
Rominos111 <romain.theodet@hotmail.fr>
2020-09-23 14:42:44 +0200

    new: Affichage byte to binaire

--- include/main.h
+++ include/main.h
@@ -11,4 +11,23 @@
 #include "map.h"
 #include "game.h"
 
+/**
+ * Pattern pour afficher un nombre binaire
+ */
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+
+/**
+ * Fonction pour afficher un nombre binaire.
+ * Usage: printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(byte));
+ */
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
 #endif //GAMEOFLIFE_MAIN_H
